8eb0a3ab977d443519f350daff9944198eec8568
Taha Asif <tahaasif11.ta@gmail.com>
2023-12-01 17:37:13 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# save this as app.py
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('weather_api.html')
+
+
+@app.route('/contact/<name>')
+def contact(name):
+    return f"<h1>Hello {name}!</h1>"
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("error_404.html")
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("error_500.html")
+
+
+app.run(debug=True)
